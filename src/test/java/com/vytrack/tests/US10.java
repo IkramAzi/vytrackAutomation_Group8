181972f85d8e6e77a8597e9d1131a8289d88ccab
ohljanzunun <zununohljan@gmail.com>
2022-03-06 20:22:17 -0500

    initial commit

---
+++ src/test/java/com/vytrack/tests/US10.java
@@ -0,0 +1,16 @@
+package com.vytrack.tests;
+
+import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class US10 {
+   public WebDriver driver;
+    @BeforeMethod
+    public void setup(){
+     //   VytrackUtils.login();
+    }
+}
+/*
+As a user, I want to write the “Description” when I create a calendar event. (Iframe)
+ */
