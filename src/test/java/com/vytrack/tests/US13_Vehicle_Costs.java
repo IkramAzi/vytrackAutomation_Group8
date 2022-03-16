538e4454951a188b22c094bd712734b820df7adc
Betul <betulkomurcu123@hotmail.com>
2022-03-16 09:10:59 -0400

    Classes done

--- src/test/java/com/vytrack/tests/US13_Vehicle_Costs.java
+++ src/test/java/com/vytrack/tests/US13_Vehicle_Costs.java
@@ -1,4 +1,82 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class US13_Vehicle_Costs {
+    @Test
+    public void Vehicle_Cost_test(){
+        //Users are on the homepage
+        VytrackUtils.loginAsDriver();
+        //Click the Vehicle Costs under the Fleet
+
+        String fleetTabElementLocator = "//span[normalize-space()='Fleet' and contains(@class,'title title-level-1')]";
+        WebElement fleetTabElement = Driver.getDriver().findElement(By.xpath(fleetTabElementLocator));
+        fleetTabElement.click();
+
+        String vehicleCostModelLocator =  "//span[normalize-space()='Vehicle Costs' and contains(@class,'title title-level-2')]";
+        WebElement vehiclesCostsElementLocator = Driver.getDriver().findElement(By.xpath(vehicleCostModelLocator));
+        BrowserUtils.sleep(1);
+        vehiclesCostsElementLocator.click();
+        BrowserUtils.sleep(1);
+        //Verify there are 3 columns on the table:
+        // Expected Column names: TYPE, TOTAL PRICE, DATE
+
+        List<String> actualColumnsTexts = new ArrayList<>(Arrays.asList("Type","Total Price", "Date"));
+
+        for (String columnsElement : actualColumnsTexts ) {
+            String columnsElementText = columnsElement;
+            actualColumnsTexts.size();
+        }
+
+        List<String> expectedcolumnaWebTable=new ArrayList<>(Arrays.asList(
+                "Type",
+                "Total Price",
+                "Date"
+        ));
+
+        Assert.assertTrue(actualColumnsTexts.equals(expectedcolumnaWebTable));
+
+    }
+
+
+    @Test
+    public void Select_All_The_Vehicle_Costs(){
+//Users are on the homepage
+        VytrackUtils.loginAsSalesManager();
+        BrowserUtils.sleep(2);
+
+// click Fleet
+        Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Fleet' and contains(@class,'title title-level-1')]")).click();
+        BrowserUtils.sleep(2);
+
+//Click the Vehicle Costs under the Fleet
+        Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Vehicle Costs' and contains(@class, 'title title-level-2')]")).click();
+        BrowserUtils.sleep(2);
+
+//Click the first checkbox
+// Verify all the vehicle costs are checked
+        Driver.getDriver().findElement(By.cssSelector(".btn.btn-default.btn-small.dropdown-toggle > input[type='checkbox']")).click();
+
+        // Driver.getDriver().findElement(By.xpath("//div[@class='btn-group dropdown']")).click();
+        WebElement checkbox = Driver.getDriver().findElement(By.xpath("//div[@class='btn-group dropdown']"));
+        checkbox.click();
+
+        Driver.getDriver().findElement(By.cssSelector("body > ul > li:nth-child(1) > a")).click();
+
+        String expectedVehicle =" all the vehicle costs are checked";
+        String actualVehicle = Driver.getDriver().getTitle();
+
+        Assert.assertEquals(Driver.getDriver().getTitle(),actualVehicle,expectedVehicle);
+    }
+
 }
--- src/test/java/com/vytrack/tests/US14Marketing_page.java
+++ src/test/java/com/vytrack/tests/US14Marketing_page.java
@@ -1,4 +1,77 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class US14Marketing_page {
+
+
+    @Test
+    public void checked_test(){
+
+        // go on home page
+        VytrackUtils.loginAsSalesManager();
+        // click Marketing
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Marketing' and contains(@class,'title title-level-1')]")).click();
+        // click Camping under Marketing
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Campaigns' and contains(@class,'title title-level-2')]")).click();
+        //click Click the Manager filters button
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//div[@class='column-manager dropdown']")).click();
+        // Verify 5 options are checked by default
+
+        WebElement DefaultCheck =Driver.getDriver().findElement(By.xpath("//span[@class='grid-header-cell__label']"));
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(DefaultCheck).pause(2).perform();
+        Assert.assertTrue(DefaultCheck.isDisplayed());
+
+
+    }
+
+    @Test
+    public void unchecked(){
+        // go on home page
+        VytrackUtils.loginAsStoreManger();
+        // click Marketing
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Marketing' and contains(@class,'title title-level-1')]")).click();
+        // click Camping under Marketing
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Campaigns' and contains(@class,'title title-level-2')]")).click();
+        //click Click the Manager filters button
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//div[@class='column-manager dropdown']")).click();
+        // Uncheck one or more filter options
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//input[@id='column-c280']")).click();
+        BrowserUtils.sleep(2);
+        Driver.getDriver().findElement(By.xpath("//input[@id='column-c283']")).click();
+
+
+        // Verify one or more options are unchecked
+        List<String> expectedUnchecked=new ArrayList<>(Arrays.asList(
+                "Name","Code", "Start Date",
+                "End Date", "Budget"));
+        List<String> actualUnchecked = new ArrayList<>(Arrays.asList("Code","Budget"));
+
+
+        Assert.assertFalse(actualUnchecked.equals(expectedUnchecked));
+
+
+
+
+    }
+
 }
